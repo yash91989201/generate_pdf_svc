badf4484f09f1ad1613de60ebe045304d4c39042
yash91989201 <yashraj6370@gmail.com>
2024-05-09 23:23:07 +0530

    fix: update puppeteer args and update pdf config to generate proper payslip pdf

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,11 +6,49 @@ export async function generatePayslipPdf(url: string) {
   const browser = await puppeteer.launch({
     headless: true,
     args: [
-      '--no-sandbox'
+      '--no-sandbox',
+      '--disable-features=IsolateOrigins',
+      '--disable-site-isolation-trials',
+      '--autoplay-policy=user-gesture-required',
+      '--disable-background-networking',
+      '--disable-background-timer-throttling',
+      '--disable-backgrounding-occluded-windows',
+      '--disable-breakpad',
+      '--disable-client-side-phishing-detection',
+      '--disable-component-update',
+      '--disable-default-apps',
+      '--disable-dev-shm-usage',
+      '--disable-domain-reliability',
+      '--disable-extensions',
+      '--disable-features=AudioServiceOutOfProcess',
+      '--disable-hang-monitor',
+      '--disable-ipc-flooding-protection',
+      '--disable-notifications',
+      '--disable-offer-store-unmasked-wallet-cards',
+      '--disable-popup-blocking',
+      '--disable-print-preview',
+      '--disable-prompt-on-repost',
+      '--disable-renderer-backgrounding',
+      '--disable-setuid-sandbox',
+      '--disable-speech-api',
+      '--disable-sync',
+      '--hide-scrollbars',
+      '--ignore-gpu-blacklist',
+      '--metrics-recording-only',
+      '--mute-audio',
+      '--no-default-browser-check',
+      '--no-first-run',
+      '--no-pings',
+      '--no-sandbox',
+      '--no-zygote',
+      '--password-store=basic',
+      '--use-gl=swiftshader',
+      '--use-mock-keychain'
     ],
     executablePath: "/usr/bin/chromium-browser",
   });
   const page = await browser.newPage();
+  await page.emulateMediaType('screen');
 
   // set header 
   page.setExtraHTTPHeaders({
@@ -20,7 +58,10 @@ export async function generatePayslipPdf(url: string) {
   await page.goto(url, { waitUntil: "domcontentloaded" });
 
   const pdf = await page.pdf({
-    format: "A4"
+    format: "A4",
+    printBackground: true,
+    tagged: true,
+    scale: 0.7
   })
 
   await browser.close()
