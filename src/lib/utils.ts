30cc45f3f7e0a8d9003bfb634b9c1509e5a38453
yash91989201 <yashraj6370@gmail.com>
2024-05-09 09:10:21 +0530

    update generatePayslip function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,12 +1,18 @@
 import puppeteer from "puppeteer";
 
 export async function generatePayslipPdf(url: string) {
-  const browser = await puppeteer.launch({ args: ['--no-sandbox'], executablePath: "/usr/bin/chromium-browser" });
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: [
+      '--no-sandbox'
+    ],
+    executablePath: "/usr/bin/chromium-browser"
+  });
   const page = await browser.newPage();
 
   await page.goto(url);
 
-  const pdf = page.pdf({
+  const pdf = await page.pdf({
     format: "A4"
   })
 
