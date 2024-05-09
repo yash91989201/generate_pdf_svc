c99d693ddef98c0a47f9881ea703267449857f1e
yash91989201 <yashraj6370@gmail.com>
2024-05-09 10:19:09 +0530

    add generate pdf header before puppeteer page.goto

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,16 +1,23 @@
 import puppeteer from "puppeteer";
 
+const GENERATE_PDF_SECRET = "SHjGXn2hjw5F1V72wY7bDXGlykN16AQACmN6tIyPrVUW"
+
 export async function generatePayslipPdf(url: string) {
   const browser = await puppeteer.launch({
     headless: true,
     args: [
       '--no-sandbox'
     ],
-    executablePath: "/usr/bin/chromium-browser"
+    executablePath: "/usr/bin/chromium-browser",
   });
   const page = await browser.newPage();
 
-  await page.goto(url);
+  // set header 
+  page.setExtraHTTPHeaders({
+    "x-generate-pdf-secret": GENERATE_PDF_SECRET
+  })
+
+  await page.goto(url, { waitUntil: "domcontentloaded" });
 
   const pdf = await page.pdf({
     format: "A4"
